291c86e4fa6c1bb39ee99496b36e5cb8a2b2975f
ZhuNingK <147676215+ZhuNingK@users.noreply.github.com>
2025-08-07 15:46:14 +0800

    1.4 封装分页面请求参数和分页返回参数

---
+++ wiki-common/src/main/java/com/wiki/core/page/PageRequest.java
@@ -0,0 +1,31 @@
+package com.wiki.core.page;
+
+
+import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+/**
+ * @author wiki
+ */
+@Data
+public class PageRequest implements Serializable {
+
+    private static final long serialVersionUID = -7947240335968370666L;
+
+    private static final Integer PAGE_NO = 1;
+    private static final Integer PAGE_SIZE = 10;
+
+    @NotNull(message = "页码不能为空")
+    @Min(value = 1, message = "页码最小值为 1")
+    private Integer pageNo = PAGE_NO;
+
+    @NotNull(message = "每页条数不能为空")
+    @Min(value = 1, message = "每页条数最小值为 1")
+    @Max(value = 100, message = "每页条数最大值为 100")
+    private Integer pageSize = PAGE_SIZE;
+
+}
